4beb0cc9965eaa03cfdada2fc0af318db5203b3a
Paul Klinger <klinger.paul@gmail.com>
2017-05-27 19:01:49 +0200

    combined input and overview, moved parsing back to App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 import logo from './logo.png';
 import './App.css';
 import StatsComponent from "./StatsComponent";
-
+import parseExport from './parseExport'
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {file: null, distribute_year: true};
+        this.state = {file: null, distribute_year: true, statistics: null};
         this.handle_options = this.handle_options.bind(this);
+        this.calc_statistics = this.calc_statistics.bind(this);
     }
 
     render() {
@@ -22,23 +23,49 @@ class App extends Component {
                     <div className="clearfloat"/>
                 </div>
                 <div id="content">
-                    <div className="InputCSV">
-                        <p>Select the .csv file exported from goodreads below.<br/>
-                            The export button can be found on <a href="https://www.goodreads.com/review/import">this page</a>,
-                            in the right column.</p>
-                        <input type="file" name="file" id="library_xml" accept=".csv"
-                               ref={(ref) => this.fileUpload = ref}
-                               onChange={this.handle_options}/>
-                        <div className="options">
-                            <label>
-                                <input name="distribute_year" type="checkbox"
-                                       checked={this.state.distribute_year} onChange={this.handle_options}/>
-                                If more than 10 books are read on Jan 1. spread them over the whole year.
-                            </label>
+                    <div id="top-bar">
+                        <div className="InputCSV">
+                            <p>Select the .csv file exported from goodreads below.<br/>
+                                The export button can be found on <a href="https://www.goodreads.com/review/import">this
+                                    page</a>,
+                                in the right column.</p>
+                            <input type="file" name="file" id="library_xml" accept=".csv"
+                                   ref={(ref) => this.fileUpload = ref}
+                                   onChange={this.handle_options}/>
+                            <div className="options">
+                                <label>
+                                    <input name="distribute_year" type="checkbox"
+                                           checked={this.state.distribute_year} onChange={this.handle_options}/>
+                                    If more than 10 books are read on Jan 1. spread them over the whole year.
+                                </label>
+                            </div>
                         </div>
+                        {this.state.statistics !== null && this.state.statistics.data.length > 0 &&
+                            <div id="overview">
+                                <div id="number_stats">
+                                    Found
+                                    <ul>
+                                        <li>{this.state.statistics.data.length + " books,"}</li>
+                                        <li>{`${this.state.statistics.data.filter(b => b.user_rating > 0).length} rated books,`}</li>
+                                        <li>{`${this.state.statistics.data_valid_date_read.length} books with finish date,`}</li>
+                                        <li>{`by ${Object.keys(this.state.statistics.author_stats).length} authors.`}</li>
+                                    </ul>
+                                </div>
+                                <div id="instructions">
+                                    <p>Click and drag on any graph to zoom in, double click to zoom out again.</p>
+                                    <p>Click on the table headings to change the sort order.</p>
+                                    <p>The .csv file exported from goodreads does not include the date you started
+                                        reading a
+                                        book,
+                                        so some of the graphs are not very accurate (e.g. pages read per day).</p>
+                                </div>
+                                <div className="clearfloat"/>
+                            </div>
+                        }
+                        <div className="clearfloat" />
                     </div>
 
-                    <StatsComponent file={this.state.file} distribute_year={this.state.distribute_year}/>
+                    <StatsComponent statistics={this.state.statistics}/>
                 </div>
             </div>
         );
@@ -47,13 +74,22 @@ class App extends Component {
     handle_options(e) {
         const target = e.target;
         if (target.name === "file") {
-            this.setState({file: this.fileUpload.files[0]});
-            return;
+            this.setState({file: this.fileUpload.files[0]}, this.calc_statistics);
+        } else {
+            const value = target.type === 'checkbox' ? target.checked : target.value;
+            this.setState({
+                [target.name]: value
+            }, this.calc_statistics);
         }
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        this.setState({
-            [target.name]: value
-        });
+    }
+
+    calc_statistics() {
+        let self = this;
+        if (this.state.file !== undefined && this.state.file !== null) {
+            parseExport(this.state.file, {distribute_year: this.state.distribute_year}).then((statistics) => {
+                self.setState({statistics: statistics});
+            });
+        }
     }
 
 }
--- src/StatsComponent.js
+++ src/StatsComponent.js
@@ -8,49 +8,21 @@ import RatingStats from './RatingStats.js'
 import ReadingStats from './ReadingStats.js'
 import AuthorStats from './AuthorStats.js'
 
-import parseExport from './parseExport'
+
 
 
 export default class StatsComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = {statistics: null}
-    }
-    componentWillReceiveProps(nextProps) {
-        let self = this;
-        let file = nextProps.file;
-        if (file !== undefined) {
-            parseExport(file, {distribute_year: nextProps.distribute_year}).then((statistics) => {
-                self.setState({statistics: statistics});
-            });
-        }
     }
 
     render() {
-        if (this.state.statistics === null) {return null}
+        if (this.props.statistics === null || this.props.statistics.data.length === 0) {return null}
         return (
-            <div id="stats" style={{display: this.state.statistics.data.length >0 ? "block" : "none"}}>
-                <div id="overview">
-                    <div id="number_stats">
-                        Found
-                        <ul>
-                            <li>{this.state.statistics.data.length + " books,"}</li>
-                            <li>{`${this.state.statistics.data.filter(b => b.user_rating > 0).length} rated books,`}</li>
-                            <li>{`${this.state.statistics.data_valid_date_read.length} books with finish date,`}</li>
-                            <li>{`by ${Object.keys(this.state.statistics.author_stats).length} authors.`}</li>
-                        </ul>
-                    </div>
-                    <div id="instructions">
-                        <p>Click and drag on any graph to zoom in, double click to zoom out again.</p>
-                        <p>Click on the table headings to change the sort order.</p>
-                        <p>The .csv file exported from goodreads does not include the date you started reading a book,
-                            so some of the graphs are not very accurate (e.g. pages read per day).</p>
-                    </div>
-                    <div className="clearfloat" />
-                </div>
-                <ReadingStats statistics={this.state.statistics} />
-                <AuthorStats statistics={this.state.statistics} />
-                <RatingStats statistics={this.state.statistics} />
+            <div id="stats">
+                <ReadingStats statistics={this.props.statistics} />
+                <AuthorStats statistics={this.props.statistics} />
+                <RatingStats statistics={this.props.statistics} />
             </div>
         );
     }
